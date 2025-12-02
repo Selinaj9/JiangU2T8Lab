774c7245c22c9c4cca6dcaca68ff6886d25fb16d
bths <sample@bths.edu>
2025-12-02 10:50:54 -0500

    StringLoops

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println();
-
-        for (int i = 0; i < 11; i++) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println();
-
-        for (int i = 15; i >= 0; i--) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println();
-
-        for (int i = 15; i > -1; i--) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println();
-
-        for (int i = 0; i <= 20; i ++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
-        }
-
-        System.out.println();
-
-        for (int i = 0; i <= 20; i += 2) {
-            System.out.print(i + " ");
-        }
+        StringLoops loops = new StringLoops();
+        System.out.println("--- testing countCharacters ---");
+        System.out.println(loops.countCharacters("a", "Apple and banana"));
+        System.out.println(loops.countCharacters("A", "Apple and banana"));
+        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.countCharacters("g", "Hello! Nice day!"));
+        System.out.println("\n--- testing reverseString ---");
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+        System.out.println(loops.reverseString("C"));
     }
 }
