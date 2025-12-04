2f67fbee0163e9846018fbb9343b2ee548b51198
bths <sample@bths.edu>
2025-12-04 11:01:06 -0500

    U2T10 removeString

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         StringLoops loops = new StringLoops();
-        System.out.println(loops.countString("an", "canned beans and bananas"));
-        System.out.println(loops.countString("tat", "Ratatattat"));
-        System.out.println(loops.countString("lower", "sunflower"));
-        System.out.println(loops.countString("haha", "Hahahahaha"));
-        System.out.println(loops.countString("rain", "rain drain pain train"));
-        System.out.println(loops.countString("was", "I was about to call you"));
-        System.out.println(loops.countString("but", "I was about to call you"));
-        System.out.println(loops.countString("i", "iiiit's supercalifragilisticexpialidociousi"));
+        System.out.println(loops.removeString("an", "canned beans and bananas"));
+        System.out.println(loops.removeString(" ", "canned beans and bananas"));
+        System.out.println(loops.removeString("tat", "Ratatattat"));
+        System.out.println(loops.removeString("lower", "sunflower"));
+        System.out.println(loops.removeString("haha", "hahahahaha"));
+        System.out.println(loops.removeString("ha", "hahahahaha"));
+        System.out.println(loops.removeString("hah", "hahahahaha"));
+        System.out.println(loops.removeString("ah", "aaaaaahhhhhh!")); // careful!
+        System.out.println(loops.removeString("rain", "it’s the brain drain pain train"));
+        System.out.println(loops.removeString("lol", "lolololol"));
+        System.out.println(loops.removeString("was", "I was about to call you"));
+        System.out.println(loops.removeString("but", "I was about to call you"));
+        System.out.println(loops.removeString("AB", "ABABABABABC"));
+        System.out.println(loops.removeString("AB", "AAAAABBBBBC"));  // careful!
+        System.out.println(loops.removeString("i", "iiiit's supercalifragilisticexpialidociousi"));
     }
 }
